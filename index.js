144edb68deb108bb4ea5b0d60d0132dae0f684a8
jose <jrepiso@itpfp.com>
2023-10-09 11:22:46 +0200

    quinto commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('hola clase')
+  res.sendFile(__dirname, Ejemplo.html)
 })
 
 app.listen(3000)
